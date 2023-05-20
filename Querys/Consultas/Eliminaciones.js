bde22643992f49a19bb01cbeb3e8642d4645d75b
Raúl <raulduranc@hotmail.com>
2023-05-20 13:51:13 +0200

    Eliminaciones

---
+++ Querys/Consultas/Eliminaciones.js
@@ -0,0 +1,49 @@
+// Seleecionamos la base de datos a utilizar.
+use("ZOO");
+
+// Realizamos consultas de filtro
+
+const borrar = db.getCollection("Animales").deleteMany({ Nombre: "Yogi" });
+
+console.log("\nBorramos los animales que se llamen Yogi\n");
+// Mostramos la colección
+console.log(`${borrar.deletedCount} animal/es borrados`);
+
+
+console.log("\n___________________________________________");
+
+// Realizamos consultas de filtro
+const borrarEspectaculos = db.getCollection("Espectaculo").deleteMany({ Aforo_Maximo: { $gte: 350 } });
+
+console.log("\nBorramos los espectáculos que tengan un aforo máximo de 350\n");
+// Mostramos la colección
+console.log(`${borrarEspectaculos.deletedCount} espectáculos borrados`);
+
+console.log("\n___________________________________________");
+
+// Realizamos consultas de filtro
+const borrarEntradas = db.getCollection("Entrada").deleteMany({$or: [{Precio: 18.75 }, {Precio:18.50}]});
+
+console.log("\nBorramos las entradas que tengan un precio de 18.75 o 18.50€\n");
+// Mostramos la colección
+console.log(`${borrarEntradas.deletedCount} entradas borradas`);
+
+console.log("\n___________________________________________");
+
+// Realizamos consultas de filtro
+const borrarAlimentacion = db.getCollection("Alimentacion").deleteMany({ Cantidad_Diaria: { $lt: 10 } });
+
+console.log("\nBorramos la alimentación con una cantidad diaria menor a 10\n");
+// Mostramos la colección
+console.log(`${borrarAlimentacion.deletedCount} alimenatcion borradas`);
+
+console.log("\n___________________________________________");
+
+// Realizamos consultas de filtro
+const borrarVeterinario = db.getCollection("Empleados").deleteOne({ Puesto: ("Veterinario") });
+
+console.log("\nBorramos un veterinario (El primero que encuentre)\n");
+// Mostramos la colección
+console.log(`${borrarVeterinario.deletedCount} veterinario borrado`);
+
+console.log("\n___________________________________________");
