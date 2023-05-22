4b32e799e88562e47e4a9ae0aed2d650b714b23e
Raúl <raulduranc@hotmail.com>
2023-05-22 18:13:38 +0200

    Correccion

--- Querys/Consultas/Modificaciones.js
+++ Querys/Consultas/Modificaciones.js
@@ -9,6 +9,7 @@ console.log("\nModificamos el animal con un id específico\n");
 console.log(`${cambioNombre.modifiedCount} modificados`);
 console.log("______________________________________\n");
 
+
 // Modificamos el salario de los que cobran menos de 2200€ incrementándolos 300€
 const subidaSalario = db.getCollection("Empleados").updateMany({Salario: {$lt:2200}},{$inc:{Salario:300}});
 
@@ -17,9 +18,10 @@ console.log("\nIncrementamos el salario\n");
 console.log(`${subidaSalario.modifiedCount} modificados`);
 console.log("______________________________________\n");
 
+// Modificamos la categoria de las  entradas que terminan en 8
 const cambioDeCategoria = db.getCollection("Entrada").updateMany({ Id_Entrada: /8$/ },{ $set: { Categoria: "ESPECIAL" } });
-  
-  // Mostramos la consulta de modificación
-  console.log("\nModificamos las entradas cuya ID terminan en 8\n");
-  console.log(`${cambioDeCategoria.modifiedCount} modificados`);
-  console.log("______________________________________\n");
+
+// Mostramos la consulta de modificación
+console.log("\nModificamos las entradas cuya ID terminan en 8\n");
+console.log(`${cambioDeCategoria.modifiedCount} modificados`);
+console.log("______________________________________\n");
