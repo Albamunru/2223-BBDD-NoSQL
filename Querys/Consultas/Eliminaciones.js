0169af278458d28cbf8e1043da7396551029fa0f
Raúl <raulduranc@hotmail.com>
2023-05-23 13:32:40 +0200

    FIN SERAFIN

--- Querys/Consultas/Eliminaciones.js
+++ Querys/Consultas/Eliminaciones.js
@@ -1,7 +1,7 @@
 // Seleecionamos la base de datos a utilizar.
 use("ZOO");
 
-// Realizamos consultas de filtro
+// 1ºRealizamos consultas de filtro
 
 const borrar = db.getCollection("Animales").deleteMany({ Nombre: "Yogi" });
 
@@ -12,7 +12,7 @@ console.log(`${borrar.deletedCount} animal/es borrados`);
 
 console.log("\n___________________________________________");
 
-// Realizamos consultas de filtro
+// 2ºRealizamos consultas de filtro
 const borrarEspectaculos = db.getCollection("Espectaculo").deleteMany({ Aforo_Maximo: { $gte: 350 } });
 
 console.log("\nBorramos los espectáculos que tengan un aforo máximo de 350\n");
@@ -21,7 +21,7 @@ console.log(`${borrarEspectaculos.deletedCount} espectáculos borrados`);
 
 console.log("\n___________________________________________");
 
-// Realizamos consultas de filtro
+// 3ºRealizamos consultas de filtro
 const borrarEntradas = db.getCollection("Entrada").deleteMany({$or: [{Precio: 18.75 }, {Precio:18.50}]});
 
 console.log("\nBorramos las entradas que tengan un precio de 18.75 o 18.50€\n");
@@ -30,7 +30,7 @@ console.log(`${borrarEntradas.deletedCount} entradas borradas`);
 
 console.log("\n___________________________________________");
 
-// Realizamos consultas de filtro
+// 4ºRealizamos consultas de filtro
 const borrarAlimentacion = db.getCollection("Alimentacion").deleteMany({ Cantidad_Diaria: { $lt: 10 } });
 
 console.log("\nBorramos la alimentación con una cantidad diaria menor a 10\n");
@@ -39,7 +39,7 @@ console.log(`${borrarAlimentacion.deletedCount} alimenatcion borradas`);
 
 console.log("\n___________________________________________");
 
-// Realizamos consultas de filtro
+// 5ºRealizamos consultas de filtro
 const borrarVeterinario = db.getCollection("Empleados").deleteOne({ Puesto: ("Veterinario") });
 
 console.log("\nBorramos un veterinario (El primero que encuentre)\n");
@@ -48,7 +48,7 @@ console.log(`${borrarVeterinario.deletedCount} veterinario borrado`);
 
 console.log("\n___________________________________________");
 
-// Realizamos consultas de filtro
+// 6ºRealizamos consultas de filtro
 const borrarEmpleado = db.getCollection("Empleados").deleteOne({ _id: ObjectId("6467a55f7593908880b5beb7") })
 
 console.log("\nBorramos al empleado con un id específico\n");
--- Querys/Consultas/Modificaciones.js
+++ Querys/Consultas/Modificaciones.js
@@ -1,8 +1,8 @@
 // Seleccionamos la base de datos.
 use('ZOO');
 
-// Modificamos la especie de la jirafa
-const cambioNombre = db.getCollection("Animales").update({Id_Animal: "JirafaM1"},{$set:{Especie:"Jirafa CuelloLargo"}});
+// 1ºModificamos la especie de la jirafa
+const cambioNombre = db.getCollection("Animales").update({Id_Animal: "JirafaM1"},{$set:{Especie:"Jirafa Cuello Largo"}});
 
 //Mostramos el número de elementos que se han modificado
 console.log("\nModificamos el animal con un id específico\n");
@@ -10,7 +10,7 @@ console.log(`${cambioNombre.modifiedCount} modificados`);
 console.log("______________________________________\n");
 
 
-// Modificamos el salario de los que cobran menos de 2200€ incrementándolos 300€
+// 2ºModificamos el salario de los que cobran menos de 2200€ incrementándolos 300€
 const subidaSalario = db.getCollection("Empleados").updateMany({Salario: {$lt:2200}},{$inc:{Salario:300}});
 
 //Mostramos el número de empleados a los que se les ha incrementado el salario
@@ -18,10 +18,47 @@ console.log("\nIncrementamos el salario\n");
 console.log(`${subidaSalario.modifiedCount} modificados`);
 console.log("______________________________________\n");
 
-// Modificamos la categoria de las  entradas que terminan en 8
+
+// 3ºModificamos la categoria de las  entradas que terminan en 8
 const cambioDeCategoria = db.getCollection("Entrada").updateMany({ Id_Entrada: /8$/ },{ $set: { Categoria: "ESPECIAL" } });
 
 // Mostramos la consulta de modificación
 console.log("\nModificamos las entradas cuya ID terminan en 8\n");
 console.log(`${cambioDeCategoria.modifiedCount} modificados`);
+console.log("______________________________________\n");
+
+
+// 4ºModificamos las alimentaciones con una cantidad diaria mayor de 50 añandiéndole un nuevo campo Observación
+const cambioAlimentacion = db.getCollection("Alimentacion").update({Cantidad_Diaria: {$gte: 50} },{$set:{Observacion:"Comen mucho"}});
+
+//Mostramos el número de elementos que se han modificado
+console.log("\nModificamos las alimentaciones con una cantidad diaria mayor de 50\n");
+console.log(`${cambioAlimentacion.modifiedCount} modificados`);
+console.log("______________________________________\n");
+
+
+// 5ºModificamos los espectáculos de aves que contengan "Av" poniéndoles un precio especial
+const cambioEspectaculo = db.getCollection("Espectaculo").update({Id_Espectaculo: /Av/},{$set:{PrecioEspecial:3}});
+
+//Mostramos el número de elementos que se han modificado
+console.log("\nModificamos los espectáculos de aves\n");
+console.log(`${cambioEspectaculo.modifiedCount} modificados`);
+console.log("______________________________________\n");
+
+
+// 6ºModificamos los hábitats de tipo tierra con los animales cuyo nombre acaben en a y le aumentamos su capacidad un 20%
+const cambioHabitat = db.getCollection("Habitat").updateMany({ Tipo: "Tierra", Nombre: /a$/ },{ $mul: { Capacidad_Maxima: 1.2 } });
+
+//Mostramos el número de elementos que se han modificado
+console.log("\nModificamos los hábitats\n");
+console.log(`${cambioHabitat.modifiedCount} modificados`);
+console.log("______________________________________\n");
+
+
+// 7ºModificamos los animales por id y le sumamos 3 años a su edad
+const cambioAnimalPorId = db.getCollection("Animales").updateMany({ _id: ObjectId("6464b25081632ad5f58db318") },{ $inc: { Edad: 3 } });
+
+//Mostramos el número de elementos que se han modificado
+console.log("\nModificamos los animales\n");
+console.log(`${cambioAnimalPorId.modifiedCount} modificados`);
 console.log("______________________________________\n");
